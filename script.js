ee883467c4b713f0778454637bf257e3f805a660
Aurelien Roux <aureroux13@me.com>
2017-02-09 14:40:11 -0500

    add comments to js file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// API INFORMATIONS ////////////////////////////////
 var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
 var DARKSKY_API_KEY = '8b3d826a8192d73fe0d71085a9b2ab06';
 var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
@@ -5,6 +6,7 @@ var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
 var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
+// GOOGLE MAPS SETTING ///////////////////////////////
 function getCoordinatesForCity(cityName){
 	var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
 
@@ -14,3 +16,6 @@ function getCoordinatesForCity(cityName){
 		.then(data => data.results[0].geometry.location)
 	);
 }
+
+// DARKSKY SETTING//////////////////////////////////////
+function
