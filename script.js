df5e9746f1a62e80d6ec4d319c7f1b228481a33c
Aurelien Roux <aureroux13@me.com>
2017-02-09 16:01:56 -0500

    js immediately invoked function expression

--- script.js
+++ script.js
@@ -1,46 +1,54 @@
-// API INFORMATIONS ////////////////////////////////
-var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
-var DARKSKY_API_KEY = '8b3d826a8192d73fe0d71085a9b2ab06';
-var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
+(function(){
+	// API INFORMATIONS ////////////////////////////////
+	var DARKSKY_API_URL = 'https://api.darksky.net/forecast/';
+	var DARKSKY_API_KEY = '8b3d826a8192d73fe0d71085a9b2ab06';
+	var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
-var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
-var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+	var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
+	var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
-// GOOGLE MAPS SETTING ///////////////////////////////
-function getCoordinatesForCity(cityName){
-	var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
+	// GOOGLE MAPS SETTING ///////////////////////////////
+	function getCoordinatesForCity(cityName){
+		var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
 
-	return(
-		fetch(url)
-		.then(response => response.json())
-		.then(data => data.results[0].geometry.location)
-	);
-}
+		return(
+			fetch(url)
+			.then(response => response.json())
+			.then(data => data.results[0].geometry.location)
+		);
+	}
 
-// DARKSKY SETTING//////////////////////////////////////
-function getCurrentWeather(coords){
-	var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
+	// DARKSKY SETTING//////////////////////////////////////
+	function getCurrentWeather(coords){
+		var url = `${CORS_PROXY}${DARKSKY_API_URL}${DARKSKY_API_KEY}/${coords.lat},${coords.lng}?units=si&exclude=minutely,hourly,daily,alerts,flags`;
 
-	return(
-		fetch(url)
-		.then(response => response.json())
-		.then(data => data.currently)
-	);
-}
+		return(
+			fetch(url)
+			.then(response => response.json())
+			.then(data => data.currently)
+		);
+	}
 
-// JAVASCRIPT UI ////////////////////////////////////
-var app = document.querySelector('#app');
-var cityForm = document.querySelector('.city-form');
-var cityInput = cityForm.querySelector('.city-input');
-var getWeatherButton = cityForm.querySelector('.get-weather-button');
-var cityWeather = app.querySelector('.city-weather');
+	// JAVASCRIPT UI ////////////////////////////////////
+	var app = document.querySelector('#app');
+	var cityForm = document.querySelector('.city-form');
+	var cityInput = cityForm.querySelector('.city-input');
+	var getWeatherButton = cityForm.querySelector('.get-weather-button');
+	var cityWeather = app.querySelector('.city-weather');
 
-getWeatherButton.addEventListener('click', function(){
-	var city = cityInput.value;
+	// USER INPUT AND TRIGGER ///////////////////////////
+	cityForm.addEventListener('submit', function(event){
+		event.preventDefault();
 
-	
-})
+		var city = cityInput.value;
 
+		getCoordinatesForCity(city)
+		.then(getCurrentWeather)
+		.then(function(weather){
+			cityWeather.innerText = 'Current temperature: ' + weather.temperature;
+		});
+	});
+})();
 
 
 
