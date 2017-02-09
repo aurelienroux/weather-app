c2d445342f254a304b0e20e20e958459da941a27
Aurelien Roux <aureroux13@me.com>
2017-02-09 14:32:15 -0500

    add function coordinates for city

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ var CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
 
 var GOOGLE_MAPS_API_KEY = 'AIzaSyDr74_EQzm3d2IXWZBCcgHWvy4DThLv9MA';
 var GOOGLE_MAPS_API_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+function getCoordinatesForCity(cityName){
+	var url = `${GOOGLE_MAPS_API_URL}?address=${cityName}&key=${GOOGLE_MAPS_API_KEY}`;
+
+	return(
+		.fetch(url)
+		.then(response => response.json())
+		.then(data => data.result[0].geometry.location)
+	);
+}
